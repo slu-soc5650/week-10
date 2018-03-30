d225225c9727196a25848bf47004b70c59564f6e
Christopher Prener <chris.prener@slu.edu>
2018-03-29 19:22:02 -0500

    save and push updated counties code

--- data/US_BOUNDARY_Counties/US_BOUNDARY_Counties.R
+++ data/US_BOUNDARY_Counties/US_BOUNDARY_Counties.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(here)
 library(tigris)
 library(sf)
@@ -6,7 +7,7 @@ library(stringr)
 counties <- counties(cb = TRUE, resolution = "20m")
 counties %>% 
   st_as_sf() %>%
-  mutate(GEOID = str_c(STATEFP, COUNTYFP)) %>%
+  mutate(GEOID = as.numeric(str_c(STATEFP, COUNTYFP))) %>%
   select(GEOID, STATEFP, COUNTYFP, NAME, ALAND) -> countiesClean
 
-
+st_write(countiesClean, here("data", "US_BOUNDARY_Counties", "US_BOUNDARY_Counties.shp"), delete_dsn = TRUE)
